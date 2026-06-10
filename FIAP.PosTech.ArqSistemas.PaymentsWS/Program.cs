9a59b610762d8f6798317f891d473f822a901727
Rodrigo Siqueira Silva <rodrigosiqueirasilva@hotmail.com>
2026-06-10 08:45:48 -0300

    Add project files.

---
+++ FIAP.PosTech.ArqSistemas.PaymentsWS/Program.cs
@@ -0,0 +1,7 @@
+using FIAP.PosTech.ArqSistemas.PaymentsWS;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
